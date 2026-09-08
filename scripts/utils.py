73e3b4a27d88240d31050b620ec47d36fc2f410e
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-08 11:46:54 +0330

    Implementing an Animation calls

--- scripts/utils.py
+++ scripts/utils.py
@@ -14,3 +14,11 @@ def load_images(path):
     for img_name in os.listdir(BASE_IMAGE_PATH + path):
         images.append(load_image(path + '/' + img_name))
     return images
+
+class Animation:
+    def __init__(self, images, img_dur=5, loop=True):
+        self.images = images
+        self.img_duration = img_dur
+        self.loop = loop
+        self.done = False
+        self.frame = 0
