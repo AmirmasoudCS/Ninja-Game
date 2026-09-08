0723ad0d72a1654c307a15ffa53862fde056e2ff
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-08 08:55:33 +0330

    Fixing a typo

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -6,8 +6,8 @@ class Tilemap:
         self.offgrid_tiles = []
 
         for i in range(10):
-            self.tilemap[str(3+i)+";10"] = {'type': 'grass', 'variant':1, 'pis': (3+i, 10)}
-            self.tilemap["10;"+str(5+i)] = {'type': 'stone', 'variant':1, 'pis': (10, 5+i)}
+            self.tilemap[str(3+i)+";10"] = {'type': 'grass', 'variant':1, 'pos': (3+i, 10)}
+            self.tilemap["10;"+str(5+i)] = {'type': 'stone', 'variant':1, 'pos': (10, 5+i)}
 
     def render(self, surf):
         for tile in self.offgrid_tiles:
