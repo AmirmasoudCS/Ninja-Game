5f845625d17348345d69769d069e35b68cbe9c5a
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-08 11:56:27 +0330

    Implementing an internal method for Animation to update based on if the Animation need loop or not

--- scripts/utils.py
+++ scripts/utils.py
@@ -29,3 +29,11 @@ class Animation:
 
     def img(self):
         return self.images[int(self.frame / self.img_duration)]
+
+    def update(self):
+        if self.loop:
+            self.frame = (self.frame + 1) % (self.img_duration * len(self.images))
+        else:
+            self.frame = min(self.frame + 1, self.img_duration * len(self.images) - 1)
+            if self.frame >= self.img_duration * len(self.images) - 1:
+                self.done = True
