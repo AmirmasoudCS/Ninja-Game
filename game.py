f8791ea09d6a405d7a897c7c8a7b9f5b2863bf52
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-08 10:17:05 +0330

    Closing the parantehsis

--- game.py
+++ game.py
@@ -38,7 +38,7 @@ class Game:
 
             self.scroll[0] += (self.player.rect().centerx -  self.display.get_width() / 2 - self.scroll[0]) / 30
             self.scroll[1] += (self.player.rect().centery -  self.display.get_height() / 2 - self.scroll[1]) / 30
-            render_scroll = int(self.scroll[0], int(self.scroll[1]))
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
 
             self.tilemap.render(self.display, offset=render_scroll)
 
