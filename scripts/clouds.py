f6acc8f793d933f964858308a5c3d0352465308a
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-08 10:31:52 +0330

    Implementing a CLouds object to contain our clouds

--- scripts/clouds.py
+++ scripts/clouds.py
@@ -13,3 +13,12 @@ class Cloud:
     def render(self, surf, offset=(0, 0)):
         render_postion = (self.pos[0] - offset[0] * self.depth, self.pos[1] - offset[1] * self.depth)
         surf.blit(self.img, (render_postion[0] % (surf.get_width() + self.img.get_width()) - self.img.get_width(), render_postion[1] % (surf.get_height() + self.img.get_height()) - self.img.get_height()))
+
+class Clouds:
+    def __init__(self, cloud_images, count=16):
+        self.clouds = []
+
+        for i in range(count):
+            self.clouds.append(Cloud((random.random() * 99999, random.random() * 99999), random.choice(cloud_images), random.random() * 0.5 + 0.05, random.random() * 0.6 + 0.2))
+
+        self.clouds.sort(key=lambda x: x.depth)
