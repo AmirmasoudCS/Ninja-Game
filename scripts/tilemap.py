8ceb8661ba94bcdca845e2d0cf694c5dd703f3ad
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-08 20:14:16 +0330

    Implementing a function to save our tilemaps

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -1,4 +1,5 @@
 import pygame
+import json
 
 NEIGHBOR_OFFSETS = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (0, 0), (-1, 1), (0, 1), (1, 1)]
 PHYSICS_TILES = {'grass', 'stone'}
@@ -18,6 +19,11 @@ class Tilemap:
             if check_loc in self.tilemap:
                 tiles.append(self.tilemap[check_loc])
         return tiles
+
+    def save(self, path):
+        f = open(path, 'w')
+        json.dump({'tilemap': self.tilemap, 'tile_size': self.tile_size, 'offgrid': self.offgrid_tiles}, f)
+        f.close()
 
     def physics_rects_around(self, pos):
         rects = []
