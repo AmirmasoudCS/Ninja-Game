8726055a329e58a28eea3e8c65d474cbd7082d49
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-08 12:14:56 +0330

    Updating the update method for the entities to take into account the flip and the animation updating

--- scripts/entities.py
+++ scripts/entities.py
@@ -57,10 +57,18 @@ class PhysicsEntity:
                     self.velocity[1] = 0
                 self.pos[1] = entity_rect.y
 
+        if movement[0] > 0:
+            self.flip = False
+        if movement[0] < 0:
+            self.flip = True
+
         self.velocity[1] = min(5, self.velocity[1] + 0.1)
 
         if self.collisions['down'] or self.collisions['up']:
             self.velocity[1] = 0
+
+        self.animation.update()
+
 
     def render(self, surf, offset=(0, 0)):
         surf.blit(pygame.transform.flip(self.animation.img(), self.flip, False), (self.pos[0] - offset[0] + self.anim_offset[0], self.pos[1] - offset[1] + self.anim_offset[1]))
