ba8dd7a0b3a01bbac4d88fef9e8f6c57bd3af52e
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-08 08:53:24 +0330

    Creating and rendering a set of tilemaps in our main Game

--- game.py
+++ game.py
@@ -3,6 +3,8 @@ import sys
 
 from scripts.entities import PhysicsEntity
 from scripts.utils import load_image, load_images
+from scripts.tilemap import Tilemap
+
 class Game:
     def __init__(self):
 
@@ -25,11 +27,14 @@ class Game:
         }
 
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
+        self.tilemap = Tilemap(tile_size=16)
 
     def run(self):
 
         while True:
             self.display.fill((14, 219, 248))
+
+            self.tilemap.render(self.display)
 
             self.player.update((self.movement[1] - self.movement[0], 0))
             self.player.render(self.display)
