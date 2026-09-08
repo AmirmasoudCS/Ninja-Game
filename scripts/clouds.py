26dfe8769e058819b0f8c4599ce2c75f6f1d7159
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-08 10:21:31 +0330

    Implementing the Cloud class

---
+++ scripts/clouds.py
@@ -0,0 +1,9 @@
+import random
+
+class Cloud:
+    def __init__(self, pos, img, speed, depth):
+        self.pos = list(pos)
+        self.img = img
+        self.speed = speed
+        self.depth = depth
+        
