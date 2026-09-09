58b3120df395f6fa6860869facc15e366d9ea990
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-09 10:02:47 +0330

    Implementing dashing on a cd

--- game.py
+++ game.py
@@ -37,6 +37,7 @@ class Game:
             'player/slide':Animation(load_images('entities/player/slide')),
             'player/wall_slide':Animation(load_images("entities/player/wall_slide")),
             'particle/leaf': Animation(load_images('particles/leaf'), img_dur=20, loop=False),
+            'particle/particle': Animation(load_images('particle/particle'), img_dur=6, loop=False),
         }
 
         self.clouds = Clouds(self.assets['clouds'], count=16)
@@ -96,6 +97,8 @@ class Game:
                         self.movement[1] = True
                     if event.key in (pygame.K_SPACE, pygame.K_UP):
                         self.player.jump()
+                    if event.key in (pygame.K_v, pygame.K_LCTRL):
+                        self.player.dash()
                 if event.type == pygame.KEYUP:
                     if event.key in (pygame.K_LEFT, pygame.K_a):
                         self.movement[0] = False
