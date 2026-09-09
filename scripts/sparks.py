4646892164a9e9346ef52e038a265d7fe6a0c98a
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-09 22:49:56 +0330

    Fixing the misscalculation of the y-axis for the Sparks

--- scripts/sparks.py
+++ scripts/sparks.py
@@ -16,10 +16,10 @@ class Spark:
 
     def render(self, surf, offset=(0, 0)):
         render_points = [
-            (self.pos[0] + math.cos(self.angle) * self.speed * 3 - offset[0], self.pos[1] + math.sin(self.angle) * self.speed * 3 - self.pos[1]),
-            (self.pos[0] + math.cos(self.angle + math.pi * 0.5) * self.speed * 0.5 - offset[0], self.pos[1] + math.sin(self.angle + math.pi * 0.5) * self.speed * 0.5 - self.pos[1]),
-            (self.pos[0] + math.cos(self.angle + math.pi) * self.speed * 3 - offset[0], self.pos[1] + math.sin(self.angle + math.pi) * self.speed * 3 - self.pos[1]),
-            (self.pos[0] + math.cos(self.angle + math.pi * 1.5) * self.speed * 0.5 - offset[0], self.pos[1] + math.sin(self.angle + math.pi * 1.5) * self.speed * 0.5 - self.pos[1]),
+            (self.pos[0] + math.cos(self.angle) * self.speed * 3 - offset[0], self.pos[1] + math.sin(self.angle) * self.speed * 3 - offset[1]),
+            (self.pos[0] + math.cos(self.angle + math.pi * 0.5) * self.speed * 0.5 - offset[0], self.pos[1] + math.sin(self.angle + math.pi * 0.5) * self.speed * 0.5 - offset[1]),
+            (self.pos[0] + math.cos(self.angle + math.pi) * self.speed * 3 - offset[0], self.pos[1] + math.sin(self.angle + math.pi) * self.speed * 3 - offset[1]),
+            (self.pos[0] + math.cos(self.angle + math.pi * 1.5) * self.speed * 0.5 - offset[0], self.pos[1] + math.sin(self.angle + math.pi * 1.5) * self.speed * 0.5 - offset[1]),
         ]
 
         pygame.draw.polygon(surf, (255, 255, 255), render_points)
