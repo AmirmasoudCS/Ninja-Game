b10532326e2fc38c94ada8b2a25fe947a18071df
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-09 09:31:05 +0330

    Implementing jump functionality to our Player class

--- game.py
+++ game.py
@@ -95,7 +95,7 @@ class Game:
                     if event.key in (pygame.K_RIGHT, pygame.K_d):
                         self.movement[1] = True
                     if event.key in (pygame.K_SPACE, pygame.K_UP):
-                        self.player.velocity[1] = -3
+                        self.player.jump()
                 if event.type == pygame.KEYUP:
                     if event.key in (pygame.K_LEFT, pygame.K_a):
                         self.movement[0] = False
