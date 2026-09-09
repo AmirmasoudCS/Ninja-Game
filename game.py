7a5159b56f72ac5277e3fcb2ffb1fef8a6be3da7
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-09 08:52:46 +0330

    Finding the leaf spawners with its offset implemented

--- game.py
+++ game.py
@@ -42,6 +42,10 @@ class Game:
 
         self.tilemap.load('map.json')
 
+        self.leaf_spawners = []
+        for tree in self.tilemap.extract([('large_decor', 2)], keep=True):
+            self.leaf_spawners.append(pygame.Rect(4 + tree['pos'][0], 4 + tree['pos'][1], 23, 13))
+
         self.scroll = [0, 0]
 
     def run(self):
