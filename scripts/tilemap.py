4eb29d3954480b09bb0f915134289db4e8819ccd
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-09 20:57:57 +0330

    Adding a method to check if a solid tile exists given a position

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -100,3 +100,9 @@ class Tilemap:
                 if loc in self.tilemap:
                     tile = self.tilemap[loc]
                     surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size - offset[0], tile['pos'][1] * self.tile_size - offset[1]))
+
+    def solid_check(self, pos):
+        tile_loc = str(int(pos[0] // self.tile_size)) + ';' + str(int(pos[1] // self.tile_size))
+        if tile_loc in self.tilemap:
+            if self.tilemap[tile_loc]['type'] in PHYSICS_TILES:
+                return self.tilemap[tile_loc]
