5ffdf88223711e56b2abce112970dc058b03e878
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-09 22:24:29 +0330

    Implementing an update method for our sparks

--- scripts/sparks.py
+++ scripts/sparks.py
@@ -6,4 +6,10 @@ class Spark:
         self.pos = list(pos)
         self.angle = angle
         self.speed = speed
-        
+
+    def update(self):
+        self.pos[0] += math.cos(self.angle) * self.speed
+        self.pos[1] += math.sin(self.angle) * self.speed
+
+        self.speed = max(0, self.speed - 0.1)
+        return not self.speed
