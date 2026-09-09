fc615b60cc5ef9150236d5c71fc948f17b934462
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-09 09:12:35 +0330

    Implementing the sin function to the decaying of the leaf particls

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import random
+import math
 
 
 from scripts.entities import PhysicsEntity, Player
@@ -79,6 +80,8 @@ class Game:
             for particle in self.particles.copy():
                 kill = particle.update()
                 particle.render(self.display, offset=render_scroll)
+                if particle.type == 'leaf':
+                    particle.pos[0] += math.sin(particle.animation.frame * 0.035) * 0.3
                 if kill:
                     self.particles.remove(particle)
 
