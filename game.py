9ec778afb6d11109c2657ba47a31b5e314fcb4db
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-09 20:49:00 +0330

    Implementing a Enemy class and adding its constructor

--- game.py
+++ game.py
@@ -38,6 +38,8 @@ class Game:
             'player/wall_slide':Animation(load_images("entities/player/wall_slide")),
             'particle/leaf': Animation(load_images('particles/leaf'), img_dur=20, loop=False),
             'particle/particle': Animation(load_images('particles/particle'), img_dur=6, loop=False),
+            'enemy/idle':Animation(load_images('entities/enemy/idle'), img_dur=6),
+            'enemy/run':Animation(load_images('entities/enemy/run'), img_dur=4),
         }
 
         self.clouds = Clouds(self.assets['clouds'], count=16)
--- scripts/entities.py
+++ scripts/entities.py
@@ -82,6 +82,11 @@ class PhysicsEntity:
     def render(self, surf, offset=(0, 0)):
         surf.blit(pygame.transform.flip(self.animation.img(), self.flip, False), (self.pos[0] - offset[0] + self.anim_offset[0], self.pos[1] - offset[1] + self.anim_offset[1]))
 
+class Enemy(PhysicsEntity):
+    def __init__(self, game, pos, size):
+        super().__init__(game, 'enemy', pos, size)
+        self.walking = 0
+
 class Player(PhysicsEntity):
     def __init__(self, game, pos, size):
         super().__init__(game, 'player', pos, size)
