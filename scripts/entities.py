8e48db081304545199f668d1c72ac459c8746b97
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-10 09:44:18 +0330

    handling the case that you are falling for so many times

--- scripts/entities.py
+++ scripts/entities.py
@@ -148,6 +148,8 @@ class Player(PhysicsEntity):
     def update(self, tilemap, movement=(0, 0)):
         super().update(tilemap, movement)
         self.air_time += 1
+        if self.air_time > 180:
+            self.game.dead += 1
         if self.collisions['down']:
             self.air_time = 0
             self.jumps = 1
