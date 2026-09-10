dd5d8aa3b13a6c2fc1709bbfba3ff5aba1cf281f
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-10 10:09:29 +0330

    Indexing

--- game.py
+++ game.py
@@ -96,7 +96,7 @@ class Game:
             if not len(self.enemies):
                 self.transition += 1
                 if self.transition > 30:
-                    self.level = min(self.level + 1, len(os.listdir('data/maps')))
+                    self.level = min(self.level + 1, len(os.listdir('data/maps')) - 1)
                     self.load_level(self.level)
             if self.transition < 0:
                 self.transition += 1
